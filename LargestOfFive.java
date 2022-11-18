e517d4870aa215a119adcdf1c726100e3b0f1920
vinit1009 <45478257+vinit1009@users.noreply.github.com>
2022-11-18 13:41:54 -0500

    Add files via upload

---
+++ LargestOfFive.java
@@ -0,0 +1,48 @@
+/*************************************************************************
+ *  Compilation:  javac LargestOfFive.java
+ *  Execution:    java LargestOfFive 35 10 32 1 8
+ *
+ *  @author:
+ *
+ *  Takes five distinct integers as command-line arguments and prints the 
+ *  largest value
+ *
+ *
+ *  % java LargestOfFive 35 10 32 1 8
+ *  35
+ *
+ *  Assume the inputs are 5 distinct integers.
+ *  Print only the largest value, nothing else.
+ *
+ *************************************************************************/
+
+public class LargestOfFive {
+
+    public static void main (String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        int d = Integer.parseInt(args[3]);
+        int e = Integer.parseInt(args[4]);
+
+        int max = a;
+        if(max < b){
+            max = b;
+        }
+        if(max < c){
+            max = c;
+        }
+        if(max < d){
+            max = d;
+        }
+        if(max < e){
+            max = e;
+        }
+
+        System.out.println(max);
+
+    
+
+        // WRITE YOUR CODE HERE
+    }
+}
